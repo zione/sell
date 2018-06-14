00f473326aca1f702475fd5d41d3eaf1d7014e60
zione <huangmayi@126.com>
2018-06-14 10:35:50 +0800

    'icon'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   },
   data () {
     return {
@@ -23,6 +26,47 @@ export default {
       {
         id: '0002',
         imgUrl: 'https://imgs.qunarzz.com/p/tts0/1511/60/4b101da07eb663f7.jpg_r_390x260x90_ce705dbf.jpg'
+      }],
+      iconList: [{
+        id: '0000',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '新马泰缅甸一日游'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
       }]
     }
   }
