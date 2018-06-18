610132f269c229374d69eae707377d1be7d2776e
zione <huangmayi@126.com>
2018-06-18 22:49:57 +0800

    'detail-list'

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,62 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <detail-list :list="list"></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './component/Banner'
 import DetailHeader from './component/header'
+import DetailList from './component/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '承认五观联票',
+          children: [
+            {
+              title: '城隍庙'
+            }
+          ]
+        },
+        {
+          title: '承认六观联票'
+        }]
+      },
+      {
+        title: '学生票',
+        children: [{
+          title: '承认三观联票'
+        },
+        {
+          title: '承认五观联票'
+        },
+        {
+          title: '承认六观联票'
+        }]
+      },
+      {
+        title: '儿童票',
+        children: [{
+          title: '承认三观联票'
+        },
+        {
+          title: '承认五观联票'
+        },
+        {
+          title: '承认六观联票'
+        }]
+      }]
+    }
   }
 }
 </script>
